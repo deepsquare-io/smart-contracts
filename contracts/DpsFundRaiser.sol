e523df49d0c5faf317a78efafec73ce1150b7a91
Julien Schneider <julien.schneider.1991@gmail.com>
2022-02-16 13:36:31 +0100

    refactor: remove unused event

--- contracts/DpsFundRaiser.sol
+++ contracts/DpsFundRaiser.sol
@@ -38,13 +38,6 @@ contract DpsFundRaiser is ReferenceTable {
     /// @notice A record of states for signing / validating signatures
     mapping(address => uint256) public nonces;
 
-    /// @notice An event thats emitted when an account changes its delegate
-    event DelegateChanged(
-        address indexed delegator,
-        address indexed fromDelegate,
-        address indexed toDelegate
-    );
-
     /// @notice An event thats emitted when a delegate account's vote balance changes
     event DelegateVotesChanged(
         address indexed delegate,
