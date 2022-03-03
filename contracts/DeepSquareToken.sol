af00063ab25b581cf1095d04676fb9bf019122c1
Julien Schneider <julien.schneider.1991@gmail.com>
2022-03-03 11:39:55 +0100

    fix: remove console.sol

--- contracts/DeepSquareToken.sol
+++ contracts/DeepSquareToken.sol
@@ -3,9 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-// TODO remove console.sol later on
-
-import "hardhat/console.sol";
 
 contract DeepSquareToken is ERC20, Ownable {
     // whitelist allowed to transfer, mint, burn
--- contracts/helper/Crowdsale.sol
+++ contracts/helper/Crowdsale.sol
@@ -7,8 +7,6 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
-// import "./ReentrancyGuard.sol";
 
 /**
  * @title Crowdsale
