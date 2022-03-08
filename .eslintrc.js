bc07230be299e3671b3b9b151efab60f00aa9937
Mathieu Bour <mathieu@squarefactory.io>
2022-03-08 22:28:35 +0100

    chore(eslint): update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,13 @@ module.exports = {
     mocha: true,
     node: true,
   },
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
+  extends: ['standard', 'plugin:prettier/recommended', 'plugin:node/recommended'],
   parserOptions: {
     ecmaVersion: 12,
   },
   overrides: [
     {
-      files: ["hardhat.config.js"],
+      files: ['hardhat.config.js'],
       globals: { task: true },
     },
   ],
