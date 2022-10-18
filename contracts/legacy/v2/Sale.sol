c78d727b65fbd9a0182d57e02d0facfab217d0fa
Lymnah <charly@mancel.net>
2022-10-18 17:44:41 +0200

    chore(legacySale): changed relative path

--- contracts/legacy/v2/Sale.sol
+++ contracts/legacy/v2/Sale.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "./Eligibility.sol";
+import "../../Eligibility.sol";
 
 /**
  * @title Token sale.
